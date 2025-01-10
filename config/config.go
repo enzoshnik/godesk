31e91a80e24a4dc2291898c4b5279dc626640ae8
Artem Evs <enzoshnik@gmail.com>
2025-01-10 14:27:40 +0300

    Step 5

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"helpdesk/models"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	dsn := "host=localhost user=postgres password=yourpassword dbname=helpdesk port=5432 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	// Миграция структуры
+	err = DB.AutoMigrate(
+		&models.User{},
+		&models.Ticket{},
+		&models.StatusRelation{},
+		&models.Status{},
+	)
+	if err != nil {
+		panic("Failed to migrate database")
+	}
+
+	(&models.Status{}).Install(DB)
+}
